81ec7d3e8e7cda7c46ff26ace4dac3061200723f
KNUCKLEBONES <joshua.paciocco@mail.utoronto.ca>
2021-06-25 15:54:10 -0400

    Testing new function

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
+from secret import password
 
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def my_function(a,b):
+    return a+b
 
 
 # Press the green button in the gutter to run the script.
@@ -17,6 +19,9 @@ if __name__ == '__main__':
     print('How are you?')
     print('What do you live?')
     print('Why?')
+    print(password)
+
+    print(my_function(2,4))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
