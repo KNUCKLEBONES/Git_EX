eeccee2d7dd7fc1e32aec83f4e7b963f3204fbcd
KNUCKLEBONES <joshua.paciocco@mail.utoronto.ca>
2021-06-25 15:26:19 -0400

    Second Commit

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     print_hi('Josh')
     print('How are you?')
     print('What do you live?')
+    print('Why?')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
